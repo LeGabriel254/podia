2288786dc0a3a0773d32e3b6a9308f4b87782b7e
Leon Gabriel <g73346920@gmail.com>
2024-08-10 14:41:36 +0300

    update all changes made

--- src/components/common/PageLayout.jsx
+++ src/components/common/PageLayout.jsx
@@ -1,3 +1,13 @@
 const PageLayout = (props) =>{
-  
+  const {children} = props;
+  return (
+    <div style={{
+      width:"100%",
+      height:'100%'
+    }}>
+{children}
+    </div>
+  );
 }
+
+export default PageLayout;
