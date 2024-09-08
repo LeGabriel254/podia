73de18a6523fa96ccda4d747f970c02a3f1953e5
Leon Gabriel <g73346920@gmail.com>
2024-09-08 23:43:44 +0300

    Chore:main.tsx added

---
+++ drop-down-navigation-app/src/components/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      <h1 className='hero font-semibold text-2xl'>Make remote work</h1>
+      <p>Get your team in sync, no matter your location.
+        Streamline processes, create team rituals and productivy soar.
+      </p>
+    </div>
+  )
+}
+
+export default Main
