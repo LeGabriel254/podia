ff2d4eefce45e56b58baf44cee7a0047c69426e6
Leon Gabriel <g73346920@gmail.com>
2024-09-11 21:33:27 +0300

    Modified

--- drop-down-navigation-app/src/components/Main.tsx
+++ drop-down-navigation-app/src/components/Main.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
+// import React from 'react'
 
-const Main = () => {
-  return (
-    <div className='container flex flex-col items-center justify-center min-h-screen'>
-      <h1 className='hero ext-4xl font-bold mb-4l'>Make remote work</h1>
-      <p className='text-lg mt-20 mr-10 text-center'>Get your team in sync, no matter your location. <br />
-        Streamline processes, create team rituals and productivy soar.
-      </p>
-    </div>
-  )
-} 
+// const Main = () => {
+//   return (
+//     <div className='container flex flex-col items-center justify-center min-h-screen'>
+//       <h1 className='hero ext-4xl font-bold mb-4l'>Make remote work</h1>
+//       <p className='text-lg mt-20 mr-10 text-center'>Get your team in sync, no matter your location. <br />
+//         Streamline processes, create team rituals and productivy soar.
+//       </p>
+//     </div>
+//   )
+// } 
 
-export default Main
+// export default Main
